efe3c8c49ffdfe7704ab2ee8e3820a479edf9d54
MotorinIlya <144696840+MotorinIlya@users.noreply.github.com>
2024-01-21 21:29:34 +0700

    Delete FileCreator.java

--- FileCreator.java
+++
@@ -1,8 +0,0 @@
-import java.io.File;
-
-public class FileCreator {
-    public static File createFile(String nameFile, String pathFile){
-        File file = new File(pathFile + "\\" + nameFile);
-        return file;
-    }
-}
